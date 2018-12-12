6d9250d9cb927762c8f8c82d38ff940b275294e8
yangang <yangangshuige@163.com>
2018-12-12 11:16:10 +0800

    初始化

---
+++ baselibrary/src/main/java/com/base/library/activity/TestActivity.java
@@ -0,0 +1,15 @@
+package com.base.library.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.base.library.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
